00e26723ba4c29848411bf4aed7df92950ec5738
ketalyn <leek105@newschool.edu>
2024-02-16 11:30:39 -0500

    again

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 function changeFont() {
-    // Select all elements with class "change-font"
     var elements = document.querySelectorAll('.Jobu');
     
-    // Array of font classes
     var fonts = ['font1', 'font2', 'font3', 'font4', 'font5', 'font6', 'font7', 'font8', 'font9', 'font10'];
 
-    // Loop through each element and assign a random font class
     elements.forEach(function(element) {
       var randomFont = fonts[Math.floor(Math.random() * fonts.length)];
-      element.className = 'Jobu ' + randomFont; // Assign new font class
+      element.className = 'Jobu ' + randomFont;
     });
   }
 
-  // Change font every couple of seconds (e.g., every 3 seconds)
-  setInterval(changeFont, 800); // Change font every 3 seconds (3000 milliseconds)
+  setInterval(changeFont, 800);
 
   let isSplit = false;
 
@@ -23,18 +19,16 @@ function splitScreen() {
     var row6 = document.getElementById('row6');
 
     if (!isSplit) {
-        // Split the screen horizontally
         videoContainer.style.display = 'flex';
         videoContainer.style.flexDirection = 'column';
         video.style.height = '50%';
-        row6.style.marginTop = '320px'; // Push down row 6
+        row6.style.marginTop = '320px';
         isSplit = true;
     } else {
-        // Revert the screen size back to its original state
         videoContainer.style.display = '';
         videoContainer.style.flexDirection = '';
         video.style.height = '';
-        row6.style.marginTop = '20px'; // Restore original margin of row 6
+        row6.style.marginTop = '20px';
         isSplit = false;
     }
 }
@@ -44,12 +38,10 @@ window.addEventListener('scroll', function() {
     var scrollHeight = document.documentElement.scrollHeight;
     var clientHeight = window.innerHeight;
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    var revealThreshold = 0.8; // Adjust the reveal threshold as needed
+    var revealThreshold = 0.8;
 
-    // Calculate how much of the page is scrolled
     var scrollPercentage = (scrollTop + clientHeight) / scrollHeight;
 
-    // Update image opacity based on scroll position
     if (scrollPercentage >= revealThreshold) {
         var opacity = (scrollPercentage - revealThreshold) / (1 - revealThreshold);
         image.style.opacity = opacity;
